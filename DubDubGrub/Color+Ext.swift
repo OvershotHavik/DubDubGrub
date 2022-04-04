7f2df643d146916da9f97bba6a0b1708f26ac450
OvershotHavik <plavetzky@gmail.com>
2022-04-04 13:37:49 -0400

    Initial atab bar setup and added assets

---
+++ DubDubGrub/Color+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  Color+Ext.swift
+//  DubDubGrub
+//
+//  Created by Steve Plavetzky on 4/4/22.
+//
+
+import SwiftUI
+
+extension Color{
+    static let brandPrimary = Color("brandPrimary")
+}
+
